ed241b92442485ec4fb693e168ffe97aaf3a03aa
Taylor Rogers <taylor.rogers71@gmail.com>
2020-02-28 16:32:05 -0800

    add model

---
+++ models/Compound.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const compoundSchema = new mongoose.Schema(
+  {
+    compound_id: { type: Number, index: true },
+    smiles: { type: String, index: true },
+    molecular_weight: { type: Number, index: true },
+    ALogP: { type: Number, index: true },
+    molecular_formula: { type: String, index: true },
+    num_rings: { type: Number, index: true },
+    image: { type: String },
+    assay_results: [
+      {
+        result_id: { type: Number, index: true },
+        target: { type: String, index: true },
+        result: { type: String, index: true },
+        operator: { type: String, index: true },
+        value: { type: Number },
+        unit: { type: String }
+      }
+    ]
+  },
+  {
+    collection: 'compounds',
+    timestamps: true
+  }
+);
+
+/**
+ * Create a global model based on the schema, and export it to be used in other files.
+ * Using serverless-offline creates issues as one call will compile the schema, and then
+ * a consecutive call will attempt to recompile the model throwing an error. Using a
+ * global model avoids this error.
+ */
+let Compound = mongoose.models.Compound || mongoose.model('Compound', compoundSchema);
+module.exports.Compound = Compound;
